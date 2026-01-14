25674c9033e60adc70159117e4dd51dbdc2cd55f
mok9617-lab <mok9617@gmail.com>
2026-01-14 12:09:23 +0900

    Update imgc_gemini copy.py

--- imgc_gemini copy.py
+++ imgc_gemini copy.py
@@ -56,7 +56,8 @@ if uploaded_file:
     st.image(img, caption = 'Uploaded Image',width='content')
     if st.button('Classification Excution'):
         with st.spinner('ing...'):
-            response = classify_image(prompt, img, model=model)
+            response = classify_image(prompt, img, model)
         st.subheader('Result')
 
         st.code(response)
+
